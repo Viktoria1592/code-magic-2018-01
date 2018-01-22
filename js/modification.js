4c6a4ca850e2a59413fa027e8781838ee1502d77
viktoria1592 <92viktori@mail.ru>
2018-01-22 19:50:03 +0300

    task1 complete, set speed, size, position

---
+++ js/modification.js
@@ -0,0 +1,18 @@
+var fireballSize = 22;
+var getFireballSpeed = function(left) {
+    return getFireballSpeed = left ? 5 : 2;  
+}
+var  wizardSpeed = 3;
+var wizardWidth = 70;
+var getWizardHeight = function() {
+    var getWizardHeight = 1.337 * wizardWidth;
+    return getWizardHeight;
+}
+function getWizardX(width) {
+    var getWizardX = 0 + width / 2;
+    return getWizardX;
+}
+function getWizardY(height) {
+    var getWizardY = 100 % - 2/3 * height;
+    return getWizardY;
+}
